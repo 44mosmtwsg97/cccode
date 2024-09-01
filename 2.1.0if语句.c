40215cf331830720430e1df21a78a137ee7b0e7f
44mosmtwsg97 <2926972979@qq.com>
2024-09-01 18:12:57 +0800

    Add files via upload

---
+++ 2.1.0if语句.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//代码1
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("未成年\n");
+//	return 0;
+//}
+
+//代码2
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int  age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("未成年\n");
+//	else
+//		printf("成年\n");
+//	return 0;
+//}
+
+//代码3
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("少年\n");
+//	else if (age >= 18 && age < 30)
+//		printf("青年\n");
+//	else if (age >= 30 && age < 50)
+//		printf("壮年\n");
+//	else if (age >= 50 && age < 80)
+//		printf("老年\n");
+//	else
+//		printf("老寿星\n");
+//	return 0;
+//}
